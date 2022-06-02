5c7fceba13470f31731d1852d5684daa6d04f44c
Sajjan Sivia <sajjansivia@gmail.com>
2022-06-02 20:27:51 +0100

    getting all data and displaying on mainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { greet } from "./utils/greet";
-
+import MainPage from "./components/MainPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <Router>
+      <>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
 export default App;
